e2273a2a6052e9549b9a84e291a86545bc476830
DThanhTrung <147610659+DThanhTrung@users.noreply.github.com>
2024-07-30 20:41:55 +0700

    Merge

--- MergeSort.c
+++ MergeSort.c
@@ -7,8 +7,13 @@
 #include <stdbool.h>
 
 #define maxn 100069
+#define inf 0x3f3f3f3f
 
-int *temp;
+int *temp, *arr;
+
+int random(int x, int y) {
+    return x + rand()%(y-x+1);
+}
 
 void Merge(int *a, int l, int r) {
     int mid=(l+r)/2;
@@ -37,10 +42,33 @@ void Merge(int *a, int l, int r) {
     for (i=l; i<=r; i++) 
         a[i]=temp[i];
 }
- 
+
+void MergeSort(int *a, int l, int r) {
+    if (l==r)
+        return;
+    int mid=(l+r)/2;
+    MergeSort(a, l, mid);
+    MergeSort(a, mid+1, r);
+    Merge(a, l, r);
+}
+
 int main() {
     srand(time(NULL));
     setvbuf(stdout, NULL, _IONBF, 0);
     temp = (int *) malloc(maxn * sizeof( int));
+    arr = (int *) malloc(maxn * sizeof( int));
+    int n;
+    scanf("%d", &n);
+    
+    for (int i=0; i<n; i++) {
+        arr[i] = random(-100, 100);
+    }
+    
+    MergeSort(arr, 0, n-1);
+
+    for (int i=0; i<n; i++)
+        printf("%d ", arr[i]);
+    free(arr);
+    free(temp);
     return 0;
 }
